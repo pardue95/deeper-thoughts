faffcc72500b4a7e7aac195f15e2a1857c1b5b7d
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-06-21 15:00:28 -0500

    implemet graphql

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,7 +21,19 @@ const typeDefs = gql`
   }
 
   type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
   }
 `;
 
